e822e6dd9146ca36bd348ae171beecf2f412ba83
gian-nis <94801100+gian-nis@users.noreply.github.com>
2021-11-21 12:29:53 -0600

    Add files via upload

---
+++ WebParsingExample.py
@@ -0,0 +1,18 @@
+
+import requests
+
+from bs4 import BeautifulSoup
+
+url = 'http://www.nytimes.com'
+
+result = requests.get(url)  # let requests method yoink url
+doc = BeautifulSoup(result.text, "html.parser")
+# result.text is to make url a text file i think and the second parameter is the type of parser you're using
+
+titles = doc.find_all(['h3', {'class': 'balancedHeadline'}])
+# in the brackets, you're putting the tag that title is listed under i think
+
+for item in titles:
+    print(item.text.strip())
+    print("---")
+# this gets whatever is in titles and takes nothing but the strings
